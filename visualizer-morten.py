ce63ef0894b9775b90169a8580d1aef0b2906944
Morten Westermann <s203965@student.dtu.dk>
2024-04-24 15:45:38 +0200

    Added new visual

---
+++ visualizer-morten.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+# from matplotlib.colors import 
+from time import perf_counter
+
+# Measre start time
+start = perf_counter()
+
+
+shape_measure = np.load('shape_measure.npy')
+best_direction = np.load('best_direction.npy')
+
+place_holder = np.ones((shape_measure.shape[0], shape_measure.shape[1], shape_measure.shape[2]), dtype=bool)
+mask = np.argmax(shape_measure, axis=3)
+mask = shape_measure[:,:,:,0] > 0.8
+mask = mask.astype(bool)
+# new_mask = np.zeros_like(mask)
+# new_mask[mask == 0] = 1
+# place_holders = np.reshape(place_holder * new_mask, place_holder.shape).astype(bool)
+# print(place_holders[0,0])
+
+test_array = np.stack([mask, mask,mask], axis=3).reshape(np.shape(best_direction)[0],np.shape(best_direction)[1],np.shape(best_direction)[2],-1).astype(bool)
+best_direction = best_direction[test_array].reshape(-1,3)
+
+max_rgb = np.max(np.abs(best_direction))
+cr = (max_rgb + best_direction[:,0]) / (2*max_rgb)
+cg = (max_rgb + best_direction[:,1]) / (2*max_rgb)
+cb = (max_rgb + best_direction[:,2]) / (2*max_rgb)
+
+
+
+# test = zip(cr.ravel(), cg.ravel(), cb.ravel())
+# print(np.array(test).shape)
+
+colorspace = [(r,b,g) for r,b,g in zip(cr.ravel(), cg.ravel(), cb.ravel())]
+place_holder[mask == False] = False
+
+
+# print(np.shape(colorspace))
+# print(np.shape(test_array))
+# print(colorspace[mask,:])
+
+# print(np.shape(colorspace[test_array]))
+
+# COLORMAP PLAYTIME
+cmap = cm.get_cmap('viridis', 3)  
+
+##########
+# 1st plot
+##########
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+sp = ax.scatter(*np.where(mask), c=colorspace, s=1, alpha=1)
+# To add a colorbar, we dont want the alpha-value to affect the colorbar, so we will plot points not in image
+# p = ax.scatter([900,900,900], [900,900,900], [900,900,900], c=[0,1,2], cmap=cmap)
+# cbar = fig.colorbar(sp, ax=ax, ticks=[0.33, 1, 1.66], alpha=1)
+# cbar.ax.set_yticklabels(['Linearity', 'Planarity', 'Sphericity'])
+# Now we have to cut out the parts we dont want to see...
+ax.set_xlim(-10,160)
+ax.set_ylim(-10, 160)
+ax.set_zlim(-10, 160)
+# Save figure. This takes time (i think because it has so many points?). This part is roughly 70 times as time-consuming as the rest.
+plt.savefig('shape_measures.png')
+print(f"1st plot took: {perf_counter() - start} sec")
+
+
+
+quit()
+##########
+# 2nd plot - Quiver for Linearity
+##########
+mask = shape_measure[:,:,:,0] > 0.8
+
+x = np.where(mask)[0]
+y = np.where(mask)[1]
+z = np.where(mask)[2]
+u = best_direction[:,:,:,0][mask].ravel()
+v = best_direction[:,:,:,1][mask].ravel()
+w = best_direction[:,:,:,2][mask].ravel()
+
+# Convert best direction to spherical coordinates
+rho = np.sqrt(u**2 + v**2 + w**2)
+theta = np.arctan2(v,u)
+phi = np.arccos(w/rho)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.quiver(x,y,z, u,v,w, length=1)
+plt.savefig('linearity_quiver.png')
+print(f"2nd plot took: {perf_counter() - start} sec")
+
